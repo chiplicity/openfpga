3f51598c6a8461cc14a837fd0bdb27738826ace0
Ganesh Gore <goreganesh007@gmail.com>
2020-11-24 18:19:02 -0700

    Updated netlist

--- FPGA88_SC_HD_Verilog/SRC/sub_module/wires.v
+++ FPGA88_SC_HD_Verilog/SRC/sub_module/wires.v
@@ -3,7 +3,7 @@
 //	Description: Wires
 //	Author: Xifan TANG
 //	Organization: University of Utah
-//	Date: Tue Nov 24 10:22:47 2020
+//	Date: Tue Nov 24 18:04:19 2020
 //-------------------------------------------
 //----- Time scale -----
 `timescale 1ns / 1ps
